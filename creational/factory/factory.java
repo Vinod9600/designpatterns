c8eff161777185b2eec3772d284ab0dfe604c7c3
Vinod Durge <vinoddurge9600@gmail.com>
2023-11-08 20:26:26 +0530

    initial Commit

---
+++ creational/factory/factory.java
@@ -0,0 +1,8 @@
+package creational.factory;
+
+public class factory {
+
+    public static void main(String[] args) {
+        System.out.println("hello factory");
+    }
+}
---
+++ creational/singleton/SingleTon.java
@@ -0,0 +1,7 @@
+package creational.singleton;
+
+public class SingleTon {
+    public static void main(String[] args) {
+        System.out.println("Hello Single Ton");
+    }
+}
